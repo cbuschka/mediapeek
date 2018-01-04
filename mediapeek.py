2803efd086a7ce07144a87864033b90056003e76
conni <c.buschka@arcusx.com>
2018-01-04 10:59:40 +0100

    Initial version.

---
+++ mediapeek.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import re
+import sys
+import urllib2
+
+def parse(file):
+  result=[]
+  entry=None
+  for line in file:
+    line = line.rstrip()
+    headMatcher = re.match('^#EXTM3U$', line)
+    valueMatcher = re.match( '^#([^\:]+):?(.*)$', line)
+    if headMatcher is not None:
+      pass
+    elif valueMatcher is not None:
+      entry={}
+      entry['type']=valueMatcher.group(1)
+      entry['params']=valueMatcher.group(2)
+      result.append(entry)
+    else:
+      entry['data']=line
+      entry={}
+  return result 
+
+response = urllib2.urlopen(sys.argv[2])
+outputFileName = sys.argv[1]
+master = parse(response)
+
+def urlByRez(master):
+  for entry in master:
+    if re.match('.*RESOLUTION=512x288.*', entry['params']) is not None:
+      return entry['data']
+
+  return None
+
+def download(outputFileName, child):
+  with open(outputFileName, 'wb') as outfile:
+    for entry in child:
+      if entry['type'] == 'EXTINF':
+        url = entry['data']
+        print('downloading ' + url)
+        fragment = urllib2.urlopen(url).read()
+        outfile.write(fragment)
+
+print(master)
+childUrl = urlByRez(master)
+child = parse(urllib2.urlopen(childUrl))
+download(outputFileName, child)
+
---
+++ readme.md
@@ -0,0 +1,17 @@
+# A (quickly written) script to grab m3u fragments from mediathek
+
+## Usage
+```
+./mediapeek.py <outfile> <url to m3u8 file> 
+```
+
+### Example
+```
+./mediapeek.py bobo-fruehstueck-und-eisdiele.mp4 'https://wdradaptiv-vh.akamaihd.net/i/medp/ondemand/weltweit/fsk0/72/720859/,720859_7736127,720859_7736126,720859_7736125,720859_7736129,720859_7736128,.mp4.csmil/master.m3u8?b=0-'
+```
+
+## Disclaimer
+Please verify if you are allowed to download content via this script.
+
+## License
+MIT
